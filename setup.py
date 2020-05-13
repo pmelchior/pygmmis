8f3d15ec1191f44bf4c544b9fa013aaddc2e673d
Peter Melchior <peter.m.melchior@gmail.com>
2020-05-13 15:38:36 -0400

    fixes #15

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('README.md').read()
 
 setup(
     name="pygmmis",
-    version='1.1.3',
+    version='1.1.4',
     description="Gaussian mixture model for incomplete, truncated, and noisy data",
     long_description = long_description,
     long_description_content_type='text/markdown',
@@ -22,5 +22,5 @@ setup(
         "Programming Language :: Python",
         "Topic :: Scientific/Engineering :: Information Analysis"
     ],
-    install_requires=["numpy","scipy","multiprocessing","parmap"]
+    install_requires=["numpy","scipy","parmap"]
 )
